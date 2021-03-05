60a5b739e0ac7f275a8b70b17b43bbc80d4618cc
youngcourtcourt <courtneynicolemacdonald@gmail.com>
2021-03-05 10:47:57 -0800

    Starting flask app

---
+++ flaskApp.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base=automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement=Base.classes.measurement
+Station=Base.classes.station
+
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
